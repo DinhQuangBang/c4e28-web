85e75d80ea8820cb0865de5b705c434641f71909
DinhQuangBang <dinhquangbangnd97@gmail.com>
2019-03-29 10:45:40 +0700

    first commit

---
+++ ex4-5-6.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+uri = "mongodb://admin:admin@ds021182.mlab.com:21182/c4e"
+client = MongoClient (uri)
+db = client.get_database ()
+rivers_collection = db ["river"]
+
+africa_continent = rivers_collection.find ({ "continent": "Africa" })
+for each_africa in africa_continent:
+    print (each_africa)
+
+print ("-" * 100)
+
+america_continent = rivers_collection.find ({ "continent": "S. America" })
+for each_america in america_continent:
+    if (each_america["length"] < 1000 ):
+        print (each_america)
